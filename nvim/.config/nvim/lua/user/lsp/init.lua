1d32280e80a4a0f27ea53dfea6d03fc6256f62a0
Justus Spitzmüller <j.spitzmueller@posteo.de>
2022-03-17 16:02:26 +0100

    ditched the lsp-installer plugin to lsp.lua

--- nvim/.config/nvim/lua/user/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
---require("user.lsp.lsp-installer")
-require'lspconfig'.clangd.setup{}
-require("user.lsp.handlers").setup()
