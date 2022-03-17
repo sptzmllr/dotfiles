f29fa0ffdd0eede5636031cbee064e11c0c108ed
Justus Spitzmüller <j.spitzmueller@posteo.de>
2022-03-17 09:28:06 +0100

    Added a spell cmp plugin

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -51,6 +51,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "f3fora/cmp-spell" -- cmd for spell
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
