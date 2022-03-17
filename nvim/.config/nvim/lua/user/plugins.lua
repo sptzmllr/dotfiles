68b5aaedae7377608d92acc9a98cecc2cb7e4c79
Justus Spitzmüller <j.spitzmueller@posteo.de>
2022-03-17 20:02:51 +0100

    Tweaked on the colorscheme

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -53,9 +53,14 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "f3fora/cmp-spell" -- cmd for spell
 
+  use "arcticicestudio/nord-vim" --nord theme 
+
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  
+  -- Treesitter
+  use{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate", }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
