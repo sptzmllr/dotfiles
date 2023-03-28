cdd7ba97b49c91c868c205aa588a3aad39f548b8
Justus Spitzmüller <j.spitzmueller@posteo.de>
2023-03-28 10:24:48 +0200

    Added python to my nvim lsp

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -41,16 +41,33 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+local lsp = require('lspconfig')
+
+lsp["clangd"].setup {
+    capabilities = capabilities,
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--suggest-missing-includes",
+      "--clang-tidy",
+      "--completion-style=bundled",
+      "--header-insertion=iwyu"
+    },
+    on_attach = on_attach,
+    flags = {debounce_text_changes = 150}
+}
+
+lsp.pyright.setup{}
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
- local servers = { 'clangd' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      ---- This will be the default in neovim 0.7+
+-- local servers = { 'clangd' }
+--for _, lsp in pairs(servers) do
+--  require('lspconfig')[lsp].setup {
+--    on_attach = on_attach,
+--    flags = {
+--      ---- This will be the default in neovim 0.7+
       --debounce_text_changes = 150,
-    }
-  }
-end
+--    }
+--  }
+--end
